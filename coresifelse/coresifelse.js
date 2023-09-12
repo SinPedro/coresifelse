73c49fc166fcb6d68205350498963fb9b0122259
Pedro de Souza Martins <143435476+SinPedro@users.noreply.github.com>
2023-09-12 15:14:24 -0300

    Add files via upload

---
+++ coresifelse/coresifelse.js
@@ -0,0 +1,40 @@
+// guardando cores e simbolos
+
+var cor = prompt("Digite a cor:")
+
+var simbolo = prompt("Digite o símbolo:")
+
+
+
+// if e else
+
+
+
+if (cor === "vermelho" && simbolo === "X") {
+    console.log("CAVE UM BURACO NO CHÃO");
+}
+
+else if (cor === "azul" && simbolo === "$"){
+    console.log("SIGA EM FRENTE");
+}
+
+else if (cor === "verde" && simbolo === "*"){
+    console.log("SUBA EM UMA ÁRVORE");
+}
+
+else if (cor === "amarelo" && simbolo === "#"){
+    console.log("DESCANSEEEE");
+}
+
+else {
+    console.log("nao há identificação disponível");
+}
+
+
+
+
+
+
+
+
+
